c79fee65f83beee6a8f575bee7dc0e9816a16d42
Makwasi <crismakwasi12@gmail.com>
2024-06-21 07:00:34 +0300

    Update sendemail.php

--- assets/inc/sendemail.php
+++ assets/inc/sendemail.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "mail@mail.com" );
+define( "RECIPIENT_NAME", "Admin" );
+define( "RECIPIENT_EMAIL", "info@pinnaclecleaners.org" );
 
 // Read the form values
 $success = false;
